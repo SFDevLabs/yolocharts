7a1bac3156318243f9f08fdbe8f3b200000b247a
Jehan <jehan.tremback@gmail.com>
2013-11-30 13:43:03 -0800

    cleanup, add scratchpad

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 /*global $*/
 
+
 $(document).ready(function(){
+  
   var chartData = '[{"day":"0","type":"foo","number":"12","rowNumber":1},{"day":"1","type":"foo","number":"23","rowNumber":2},{"day":"2","type":"foo","number":"16","rowNumber":3},{"day":"3","type":"foo","number":"45","rowNumber":4},{"day":"4","type":"foo","number":"23","rowNumber":5},{"day":"5","type":"foo","number":"43","rowNumber":6},{"day":"6","type":"foo","number":"54","rowNumber":7},{"day":"7","type":"foo","number":"23","rowNumber":8},{"day":"8","type":"foo","number":"56","rowNumber":9},{"day":"9","type":"foo","number":"65","rowNumber":10},{"day":"0","type":"bar","number":"13","rowNumber":11},{"day":"1","type":"bar","number":"15","rowNumber":12},{"day":"2","type":"bar","number":"42","rowNumber":13},{"day":"3","type":"bar","number":"54","rowNumber":14},{"day":"4","type":"bar","number":"32","rowNumber":15},{"day":"5","type":"bar","number":"12","rowNumber":16},{"day":"6","type":"bar","number":"23","rowNumber":17},{"day":"7","type":"bar","number":"12","rowNumber":18},{"day":"8","type":"bar","number":"12","rowNumber":19},{"day":"9","type":"bar","number":"22","rowNumber":20}]';
 
   var setBounds = '60, 30, 505, 105';
 
   var xAxis = 'day';
+
+  $("#example").handsontable({
+      data: chartData,
+      startRows: 5,
+      startCols: 5,
+      minSpareCols: 1,
+      //always keep at least 1 spare row at the right
+      minSpareRows: 1,
+      //always keep at least 1 spare row at the bottom,
+      rowHeaders: true,
+      colHeaders: true,
+      contextMenu: true
+  });
 
   function template(chartData, setBounds, xAxis) {
     return '<div id="yolochart">' +
