ebf592954fbc5a16544afecdc48e7328d5a26b94
Jehan <jehan.tremback@gmail.com>
2013-12-06 20:24:13 -0800

    refactor runs

--- script.js
+++ script.js
@@ -9,16 +9,26 @@
 //
 //
 
-/*global $, _, console*/
+/*global $, _, console, templates*/
 
 var exampleData = [
-  ["Year", "Maserati", "Mazda", "Mercedes", "Mini", "Mitsubishi"],
-  ["2009", "0", "2941", "4303", "354", "5814"],
-  ["2010", "5", "2905", "2867", "412", "5284"],
-  ["2011", "4", "2517", "4822", "552", "6127"],
-  ["2012", "2", "2422", "5399", "776", "4151"],
+  ['Year', 'Maserati', 'Mazda', 'Mercedes', 'Mini', 'Mitsubishi'],
+  ['2009', '0', '2941', '4303', '354', '5814'],
+  ['2010', '5', '2905', '2867', '412', '5284'],
+  ['2011', '4', '2517', '4822', '552', '6127'],
+  ['2012', '2', '2422', '5399', '776', '4151'],
   [null, null, null, null, null, null]
 ];
+
+function generateUUID() {
+    var d = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (d + Math.random()*16)%16 | 0;
+        d = Math.floor(d/16);
+        return (c=='x' ? r : (r&0x7|0x8)).toString(16);
+    });
+    return uuid;
+}
 
 //eliminates any rows that contain only falsey values
 function trimEmptyRows(input) {
@@ -39,15 +49,20 @@ function innerCompact(input) {
   return output;
 }
 
-function hotToD3(input, y_units) {
+function hotToD3(input, y_axis_key) {
+  
   //clean out empty columns and rows
   var clean = innerCompact(trimEmptyRows(input)),
       //remove and store header row
       header = clean.splice(0, 1)[0],
       //remove and store x axis key
       x_axis_key = header.splice(0, 1)[0],
-      //init output array
-      output = [];
+      //init output obj
+      output = {
+        data: [],
+        x_axis_key: x_axis_key,
+        y_axis_key: y_axis_key
+      };
 
   _.forEach(clean, function(row){
     // get value of x-axis, per row
@@ -61,8 +76,8 @@ function hotToD3(input, y_units) {
       // so we set 'series' as the label for that column from the header
       doc['series'] = value;
       // get the value from the row, and put it in the field corresponding to the units the user has chosen
-      doc[y_units] = row[index];
-      output.push(doc);
+      doc[y_axis_key] = row[index];
+      output.data.push(doc);
     });
   });
 
@@ -70,47 +85,57 @@ function hotToD3(input, y_units) {
 }
 
 
-function Table(elements) {
+function Table(opts) {
   var self = $.observable(this);
 
-  self.elements = elements;
+  self.opts = opts;
 
   self.draw = function(data) {
-    self.elements.table_box.handsontable({
+    self.opts.table_box.handsontable({
       data: data,
-      minRows: 20,
-      minCols: 20,
       minSpareCols: 1,
       minSpareRows: 1,
       rowHeaders: false,
       colHeaders: false,
       contextMenu: true,
       afterChange: function() {
-        self.trigger('updated', self.getData);
+        self.trigger('updated', self.getData());
       }
     });
   };
 
   self.getData = function() {
-    var instance = self.elements.table_box.handsontable('getInstance');
+    var instance = self.opts.table_box.handsontable('getInstance');
     return {
       table_data: instance.getData(),
-      y_units: self.elements.units_field.val()
+      y_axis_key: self.opts.units_field.val()
     };
   };
 }
 
 
-function Chart(elements) {
+function Chart(opts) {
   var self = $.observable(this);
 
-  self.elements = elements;
+  self.settings = opts;
 
   self.draw = function(opts) {
-    var data = hotToD3(opts.table_data, opts.y_units);
+    var conversion = hotToD3(opts.table_data, opts.y_axis_key),
+        template_vars = {
+          chart_data: JSON.stringify(conversion.data),
+          chart_id: 'chartable-' + generateUUID(),
+          x_axis_key: conversion.x_axis_key,
+          y_axis_key: conversion.y_axis_key
+        };
 
-    self.elements.chart_box.replaceWith($.render(self.elements.template.text(), data));
-    self.elements.embed_box.text($.render(self.elements.template.text(), data));
+    // template_vars.chart_data = JSON.stringify(hotToD3(opts.table_data, opts.y_axis_key));
+    
+    var rendered = $.render(self.settings.template, template_vars);
+
+    console.log(template_vars, rendered);
+
+    self.settings.chart_box.html(rendered);
+    self.settings.embed_box.text(rendered);
   };
 }
 
@@ -124,13 +149,13 @@ function initEditor(hash) {
 
   // initialize chart with its elements
   var chart = new Chart({
-    template: $('#' + hash + '_template'),
+    template: templates.stacked_area,
     chart_box: $('#js-chart'),
     embed_box: $('#js-embed')
   });
 
-  // gets example data from dom el, stringifies, and draws table with it
-  table.draw(JSON.parse($('#' + hash + '_data').text()));
+  // draw table with example data
+  table.draw(exampleData);
   
   // when table is updated, draw the chart with the right data
   table.on('updated', function(opts){
@@ -141,4 +166,4 @@ function initEditor(hash) {
 
 $.route(function(hash) {
   initEditor(hash);
-};
+});
