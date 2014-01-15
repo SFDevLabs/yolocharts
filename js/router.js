3caf82923efe936b481bc1fa7b73f79f0b52afe0
Jehan <jehan.tremback@gmail.com>
2014-01-14 21:18:34 -0800

    switch to single style chart

--- js/router.js
+++ js/router.js
@@ -7,16 +7,19 @@ var table,
 
 var trim_hash = hash.slice(1);
 
-if (!templates[trim_hash]) {
-  trim_hash = 'simple_area';
-  window.location.hash = trim_hash;
-}
+// conssole.log(trim_hash)
+
+// if (!templates[trim_hash]) {
+//   trim_hash = 'simple_area';
+//   window.location.hash = trim_hash;
+// }
 
 var chosen_template = _.where(templates, { name: trim_hash })[0];
 
 table = new Table(chosen_template);
 chart = new Chart(templates, trim_hash);
-chartCarousel($('#js-chart-carousel'), $('#js-embed-box'));
+
+chartBox($('#js-chart-box'), $('#js-embed-box'));
 tableBox($('#js-table-box'));
 
 chart.on('type_change', function(type) {
